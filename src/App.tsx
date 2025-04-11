99b70873ca8b84f239bd33c34326a5b464a9f62e
lucasfontinele <contato@fontinele.dev>
2025-04-11 10:31:17 -0300

    feat: :wrench: implements Origin UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import EventCalendarPage from './shared/components/comp-542';
 
 function App() {
   return (
     <div>
       <p>Hello world</p>
+      <EventCalendarPage />
     </div>
   );
 }
